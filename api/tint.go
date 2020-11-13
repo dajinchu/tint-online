5d72645b26b7017a8414f40c0c58910f8281a0e4
dajinchu <dajinchu@github.com>
2020-11-13 16:33:51 -0500

    test

--- api/tint.go
+++ api/tint.go
@@ -4,9 +4,8 @@ import (
   "fmt"
 	"net/http"
 	"io/ioutil"
-	// "gopkg.in/yaml.v2"
-	//  "github.com/dajinchu/tint/builder/yaml"
-	//  "github.com/dajinchu/tint/machine"
+	builder "github.com/dajinchu/tint/builder/yaml"
+	"github.com/cjcodell1/tint/machine"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -14,8 +13,42 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
     panic(err)
 	}
-
 	fmt.Printf("%s", body)
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-	// m, err = yaml.Build(config, machineFlag)
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	var m machine.Machine
+	var machineType = "one-way-tm"
+	m, err = builder.Build(string(body), machineType)
+	fmt.Fprintf(w, "%d", test(m, "0 1 0"))
+}
+
+type Result int
+
+const (
+	Accept Result = 1
+  Reject = 0
+	Error = -1
+)
+
+func test(m machine.Machine, input string) Result {
+	var err error
+	conf := m.Start(input)
+	for {
+		// print verbosely
+		// if verboseFlag {
+		// 	fmt.Println(conf.Print())
+		// }
+
+		// check if accept or reject and break
+		if m.IsAccept(conf) {
+			return 1
+		} else if m.IsReject(conf) {
+			return 0
+		}
+
+		// step
+		conf, err = m.Step(conf)
+		if err != nil {
+			return -1
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/dajinchu/tint-online
 go 1.15
 
 require (
-	github.com/cjcodell1/tint v0.0.0-20200604191417-b63be41b2b0d // indirect
+	github.com/cjcodell1/tint v0.0.0-20200604191417-b63be41b2b0d
 	github.com/dajinchu/tint v0.0.0-20201113211847-e92b16504819
 	gopkg.in/yaml.v2 v2.3.0
 )
